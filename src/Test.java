b8526e13cdfd0f019cac194eab2c1232c2736fee
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2021-11-01 22:18:30 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,40 @@
+/**
+ *
+ * @author Thiwanka
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+abstract class computerType{
+    Brand brand;
+    Model model;
+}
+
+class desktopPc extends computerType{
+    
+}
+class laptopPc extends computerType{
+    
+}
+interface Brand{
+    
+}
+class MSI implements Brand{
+    
+}
+class Acer implements Brand{
+    
+}
+interface Model{
+    
+}
+class X592 implements Model{
+    
+}
+class Z124 implements Model{
+    
+}
