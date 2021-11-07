3c531b852b6b127ec8147e8385f2fcaa66c9ffa4
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2021-11-07 10:17:36 +0530

    first commit

--- src/Test.java
+++ src/Test.java
@@ -10,31 +10,80 @@ public class Test {
 }
 
 abstract class computerType{
+    
+    String RAM = "8GB";
+    String GPU = "Nvidia x20 2GB";
+    String SSD = "Kingston 250GB";
+    
     Brand brand;
     Model model;
+
+    public computerType(Brand brand,Model model) {
+    }
+ 
+public abstract void createCom();
 }
 
+
 class desktopPc extends computerType{
+
+    public desktopPc(Brand brand,Model model) {
+        super(brand,model);
+    }
+    
+    @Override
+    public void createCom() {
+        
+    }
     
 }
 class laptopPc extends computerType{
+
+    public laptopPc(Brand brand,Model model) {
+        super(brand,model);
+    }
+  
+    @Override
+    public void createCom() {
+        
+    }
     
 }
 interface Brand{
-    
+   public abstract void fillBrand(); 
 }
 class MSI implements Brand{
+
+    @Override
+    public void fillBrand() {
+        
+    }
     
 }
 class Acer implements Brand{
+
+    @Override
+    public void fillBrand() {
+        
+    }
     
 }
 interface Model{
-    
+    public abstract void fillModel();
 }
 class X592 implements Model{
+
+    @Override
+    public void fillModel() {
+        
+    }
     
 }
 class Z124 implements Model{
+
+    @Override
+    public void fillModel() {
+        
+    }
     
 }
